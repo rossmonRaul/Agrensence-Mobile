a0bd70178c6baa4858003d52950346383f294bcd
Kendall Madrigal <kendallmadrigal14@gmail.com>
2024-02-12 10:04:25 -0600

    Archivo de las constantes

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+// Se definen las constantes de los nombres de las pantallas por si luego se cambian de nombre las pantallas
+export const Screen_Names = {
+    Login: 'Login',
+    Register: 'Register',
+};
+
+// Esta es la IP que uno debe cambiar en local por la IP de cada uno
+export const IP_API = '192.168.0.15';
