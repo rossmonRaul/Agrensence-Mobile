14727d6041f2dd2aa324e012282854dc3e67df4d
Kendall Madrigal <kendallmadrigal14@gmail.com>
2024-02-19 13:31:27 -0600

    servicio de usuario actualizado

--- src/servicios/ServicioUsuario.ts
+++ src/servicios/ServicioUsuario.ts
@@ -6,11 +6,26 @@ export const ObtenerUsuarios = async () => {
     const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ObtenerUsuarios`;
     return await ProcesarDatosApi('GET', url, '');
 }
+export const AdminInsertarUsuario = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/GuardarUsuarioPorSuperUsuario`;
+    return await ProcesarDatosApi('POST', url, data);
+}
+
+export const ObtenerUsuariosRolNoAsignado = async () => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ObtenerUsuariosPorRol4`;
+    return await ProcesarDatosApi('GET', url, '');
+}
 
 export const InsertarUsuario = async (data: any) => {
     const url = `http://${IP_API}:5271/api/v1.0/${controlador}/InsertarUsuario`;
     return await ProcesarDatosApi('POST', url, data);
 }
+
+export const ActualizarUsuario = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ActualizarUsuario`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
+
 
 export const ValidarUsuario = async (data: any) => {
     const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ValidarUsuario`;
