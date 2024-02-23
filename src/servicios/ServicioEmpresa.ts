cbbed3789d5694a1b09c60a50f50a2ebee4146b3
Kendall Madrigal <kendallmadrigal14@gmail.com>
2024-02-23 14:01:36 -0600

    Servicios actualizados conforme el backend

--- src/servicios/ServicioEmpresa.ts
+++ src/servicios/ServicioEmpresa.ts
@@ -1,8 +1,25 @@
 import { ProcesarDatosApi } from "./ApiFetch";
 const controlador = "Empresa";
 import { IP_API } from "../constants";
-
+/*Metodos GET */
 export const ObtenerEmpresas = async () => {
     const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ObtenerEmpresas`;
     return await ProcesarDatosApi('GET', url, '');
+}
+
+/*Metodos POST */
+export const InsertarEmpresa = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/CrearEmpresa`;
+    return await ProcesarDatosApi('POST', url, data);
+}
+
+/*Metodos PUT */
+export const ModificarEmpresa = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ActualizarEmpresa`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
+
+export const CambiarEstadoEmpresa = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/CambiarEstadoEmpresa`;
+    return await ProcesarDatosApi('PUT', url, data);
 }
--- src/servicios/ServicioUsuario.ts
+++ src/servicios/ServicioUsuario.ts
@@ -1,33 +1,75 @@
 import { ProcesarDatosApi } from "./ApiFetch";
 const controlador = "Usuario";
 import { IP_API } from "../constants";
-
+/*Metodos GET */
 export const ObtenerUsuarios = async () => {
     const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ObtenerUsuarios`;
     return await ProcesarDatosApi('GET', url, '');
 }
-export const AdminInsertarUsuario = async (data: any) => {
-    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/GuardarUsuarioPorSuperUsuario`;
-    return await ProcesarDatosApi('POST', url, data);
+export const ObtenerUsuariosPorRol2 = async () => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ObtenerUsuariosPorRol2`;
+    return await ProcesarDatosApi('GET', url, '');
 }
-
-export const ObtenerUsuariosRolNoAsignado = async () => {
+export const ObtenerUsuariosPorRol4 = async () => {
     const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ObtenerUsuariosPorRol4`;
     return await ProcesarDatosApi('GET', url, '');
 }
 
-export const InsertarUsuario = async (data: any) => {
-    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/InsertarUsuario`;
+/*Metodos POST */
+
+export const GuardarUsuarioPorSuperUsuario = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/GuardarUsuarioPorSuperUsuario`;
     return await ProcesarDatosApi('POST', url, data);
 }
-
-export const ActualizarUsuario = async (data: any) => {
-    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ActualizarUsuario`;
-    return await ProcesarDatosApi('PUT', url, data);
+export const GuardarUsuario = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/GuardarUsuario`;
+    return await ProcesarDatosApi('POST', url, data);
 }
-
-
 export const ValidarUsuario = async (data: any) => {
     const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ValidarUsuario`;
     return await ProcesarDatosApi('POST', url, data);
 }
+export const ObtenerUsuariosPorRol3 = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ObtenerUsuariosPorRol3`;
+    return await ProcesarDatosApi('POST', url, data);
+}
+export const ObtenerUsuariosPorIdEmpresa = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ObtenerUsuariosPorIdEmpresa`;
+    return await ProcesarDatosApi('POST', url, data);
+}
+
+/*Metodos 
+/*Metodos PUT */
+export const ActualizarContrasenaUsuario = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ActualizarContrasenaUsuario`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
+export const ActualizarUsuarioAdministrador = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/ActualizarUsuarioAdministrador`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
+
+export const AsignarFincaParcela = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/AsignarFincaParcela`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
+export const AsignarEmpresaFincaYParcela = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/AsignarEmpresaFincaYParcela`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
+
+export const AsignarNuevaFincaParcela = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/AsignarNuevaFincaParcela`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
+export const CambiarEstadoUsuarioFincaParcela = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/CambiarEstadoUsuarioFincaParcela`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
+
+export const CambiarEstadoUsuario = async (data: any) => {
+    const url = `http://${IP_API}:5271/api/v1.0/${controlador}/CambiarEstadoUsuario`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
+
+
