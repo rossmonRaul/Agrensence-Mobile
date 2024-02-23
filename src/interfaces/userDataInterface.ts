23feaa249af0855947761b54b64c66558c71e2a3
Kendall Madrigal <kendallmadrigal14@gmail.com>
2024-02-23 14:02:44 -0600

    Se agregan interfaces

---
+++ src/interfaces/userDataInterface.ts
@@ -0,0 +1,9 @@
+export interface UserDataInterface {
+    identificacion: string;
+    correo: string;
+    idEmpresa: number;
+    idFinca: number;
+    idParcela: number;
+    idRol: number;
+    estado: boolean;
+}
