765df9ac98377d66d9d4fc7414951343b5a904ce
Kendall Madrigal <kendallmadrigal14@gmail.com>
2024-02-26 14:50:10 -0600

    Cmabiando la configuracion de los roles

--- src/constants.ts
+++ src/constants.ts
@@ -13,6 +13,7 @@ export const ScreenProps = {
     AdminModifyUser: { screenName: 'AdminModifyUser' },
     AdminModifyAdminUser: { screenName: 'AdminModifyAdminUser' },
     AdminUserList: { screenName: 'AdminUserList' },
+    AdminCreateCompany: { screenName: 'AdminCreateCompany' },
 };
 
 // Esta es la IP que uno debe cambiar en local por la IP de cada uno
@@ -20,6 +21,7 @@ export const IP_API = '192.168.0.2';
 
 // Se definen las constantes de las imagenes y texto que va en los cuadros de la pantalla Empresa
 export const Company_Props = [
+    //Usuarios
     { id: 1, iconImage: require('./assets/images/icons/suelos.png'), text: 'Suelos', screen: '' },
     { id: 2, iconImage: require('./assets/images/icons/cultivos.png'), text: 'Cultivos', screen: '' },
     { id: 3, iconImage: require('./assets/images/icons/hidrico.png'), text: 'Hídrico', screen: '' },
@@ -30,13 +32,20 @@ export const Company_Props = [
     { id: 8, iconImage: require('./assets/images/icons/recomendacion.png'), text: 'Recomendación', screen: '' },
     { id: 9, iconImage: require('./assets/images/icons/calidad.png'), text: ' Control de Calidad', screen: '' },
     { id: 10, iconImage: require('./assets/images/icons/preferencias.png'), text: 'Preferencias', screen: '' },
-    { id: 11, iconImage: require('./assets/images/icons/editar-usuario.png'), text: 'Asignar finca y parcela', screen: ScreenProps.AdminUserList.screenName, datoValidacion: '0' },
-    { id: 12, iconImage: require('./assets/images/icons/asignar-usuario.png'), text: 'Asignar usuarios', screen: ScreenProps.ListUsersRol4.screenName },
-    { id: 13, iconImage: require('./assets/images/icons/editar-usuario.png'), text: 'Panel de usuarios', screen: ScreenProps.AdminUserList.screenName, datoValidacion: '1' },
-    { id: 14, iconImage: require('./assets/images/icons/crear-usuario.png'), text: 'Crear usuario administrador', screen: ScreenProps.AdminRegisterUser.screenName },
-    { id: 15, iconImage: require('./assets/images/icons/lista-companias.png'), text: 'Lista de compañias', screen: ScreenProps.CompanyList.screenName },
-    { id: 16, iconImage: '', text: '', screen: '' }
+
+    //Administrador
+    { id: 50, iconImage: require('./assets/images/icons/lista-usuarios-habilitados.png'), text: 'Lista usuarios habilitados', screen: ScreenProps.AdminUserList.screenName, datoValidacion: '0' },
+    { id: 51, iconImage: require('./assets/images/icons/asignar-usuario.png'), text: 'Habilitar usuarios', screen: ScreenProps.ListUsersRol4.screenName },
+    { id: 52, iconImage: require('./assets/images/icons/editar-usuario.png'), text: 'Panel de usuarios', screen: ScreenProps.AdminUserList.screenName, datoValidacion: '1' },
+
+    //SuperAdmin
+    { id: 100, iconImage: require('./assets/images/icons/crear-usuario.png'), text: 'Crear usuario administrador', screen: ScreenProps.AdminRegisterUser.screenName },
+    { id: 101, iconImage: require('./assets/images/icons/lista-companias.png'), text: 'Lista de empresas', screen: ScreenProps.CompanyList.screenName },
+    { id: 102, iconImage: require('./assets/images/icons/crear-compania.png'), text: 'Crear empresa', screen: ScreenProps.AdminRegisterCompany.screenName },
+    { id: 103, iconImage: require('./assets/images/icons/panel-usuarios-administradores.png'), text: 'Panel usuarios administradores', screen: ScreenProps.AdminUserList.screenName, datoValidacion: '0' },
+    { id: 200, iconImage: '', text: '', screen: '' }
 ]
+
 export const Admin_cultivation = [
     { id: 1, iconImage: require('./assets/images/icons/rotacion-cultivos.png'), text: 'Rotación de cultivos según estacionalidad' },
     { id: 2, iconImage: require('./assets/images/icons/productividad.png'), text: 'Productividad de cultivo' },
