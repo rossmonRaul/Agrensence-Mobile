c6d8d897a434b3789a3f310786fde175b1330cac
def-jota21 <josejuliarm8@gmail.com>
2024-03-07 10:06:51 -0600

    constants para los screen

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,7 @@ export const ScreenProps = {
     Login: { screenName: 'Login' },
     Register: { screenName: 'Register' },
     Menu: { screenName: 'Menu' },
+    MenuFloor: { screenName: 'MenuSueloScreen' },
     AdminRegisterUser: { screenName: 'AdminRegisterUser' },
     AssignCompany: { screenName: 'AssignCompany' },
     CompanyList: { screenName: 'CompanyList' },
@@ -12,20 +13,27 @@ export const ScreenProps = {
     ListUsersRol4: { screenName: 'ListUsersRol4' },
     AdminModifyUser: { screenName: 'AdminModifyUser' },
     AdminModifyAdminUser: { screenName: 'AdminModifyAdminUser' },
+    AdminCrops: { screenName: 'AdministracionCultivos' },
     AdminUserList: { screenName: 'AdminUserList' },
     AdminCreateCompany: { screenName: 'AdminCreateCompany' },
+    ListEstate: { screenName: 'ListEstate' },
+    RegisterEstate: { screenName: 'RegistrarFincaScreen' },
+    ModifyEstate: { screenName: 'ModificarFincaScreen' },
+    ListPlot: { screenName: 'ListaParcelasScreen' },
+    RegisterPlot: { screenName: 'RegistrarParcelaScreen' },
+    ModifyPlot: { screenName: 'ModificarParcelaScreen' },
 };
 
 // Esta es la IP que uno debe cambiar en local por la IP de cada uno
-export const IP_API = '192.168.0.2';
+export const IP_API = '192.168.1.35';
 
 export const API_URL = `http://${IP_API}:5271`
 
 // Se definen las constantes de las imagenes y texto que va en los cuadros de la pantalla Empresa
 export const Company_Props = [
     //Usuarios
-    { id: 1, iconImage: require('./assets/images/icons/suelos.png'), text: 'Suelos', screen: '' },
-    { id: 2, iconImage: require('./assets/images/icons/cultivos.png'), text: 'Cultivos', screen: '' },
+    { id: 1, iconImage: require('./assets/images/icons/suelos.png'), text: 'Suelos', screen: ScreenProps.MenuFloor.screenName},
+    { id: 2, iconImage: require('./assets/images/icons/cultivos.png'), text: 'Cultivos', screen: ScreenProps.AdminCrops.screenName },
     { id: 3, iconImage: require('./assets/images/icons/hidrico.png'), text: 'Hídrico', screen: '' },
     { id: 4, iconImage: require('./assets/images/icons/clima.png'), text: 'Clima', screen: '' },
     { id: 5, iconImage: require('./assets/images/icons/plagas.png'), text: 'Plagas', screen: '' },
@@ -37,8 +45,10 @@ export const Company_Props = [
 
     //Administrador
     { id: 50, iconImage: require('./assets/images/icons/lista-usuarios-habilitados.png'), text: 'Lista usuarios habilitados', screen: ScreenProps.AdminUserList.screenName, datoValidacion: '0' },
-    { id: 51, iconImage: require('./assets/images/icons/asignar-usuario.png'), text: 'Habilitar usuarios', screen: ScreenProps.ListUsersRol4.screenName },
+    //{ id: 51, iconImage: require('./assets/images/icons/asignar-usuario.png'), text: 'Habilitar usuarios', screen: ScreenProps.ListUsersRol4.screenName },
     { id: 52, iconImage: require('./assets/images/icons/editar-usuario.png'), text: 'Panel de usuarios', screen: ScreenProps.AdminUserList.screenName, datoValidacion: '1' },
+    { id: 53, iconImage: require('./assets/images/icons/campo.png'), text: 'Lista de Fincas', screen: ScreenProps.ListEstate.screenName, datoValidacion: '1' },
+    { id: 54, iconImage: require('./assets/images/icons/parcela.png'), text: 'Lista de Parcelas', screen: ScreenProps.ListPlot.screenName, datoValidacion: '1' },
 
     //SuperAdmin
     { id: 101, iconImage: require('./assets/images/icons/lista-companias.png'), text: 'Panel de empresas', screen: ScreenProps.CompanyList.screenName },
@@ -47,6 +57,13 @@ export const Company_Props = [
 ]
 
 export const Admin_cultivation = [
+    { id: 1, iconImage: require('./assets/images/icons/rotacion-cultivos.png'), text: 'Rotación de cultivos según estacionalidad' },
+    { id: 2, iconImage: require('./assets/images/icons/productividad.png'), text: 'Productividad de cultivo' },
+    { id: 3, iconImage: require('./assets/images/icons/montana.png'), text: 'Preparación del terreno y prácticas de conservación' },
+    { id: 4, iconImage: require('./assets/images/icons/historial.png'), text: 'Historial de manejo  de residuos de la finca' },
+]
+
+export const Admin_suelo = [
     { id: 1, iconImage: require('./assets/images/icons/rotacion-cultivos.png'), text: 'Rotación de cultivos según estacionalidad' },
     { id: 2, iconImage: require('./assets/images/icons/productividad.png'), text: 'Productividad de cultivo' },
     { id: 3, iconImage: require('./assets/images/icons/montana.png'), text: 'Preparación del terreno y prácticas de conservación' },
