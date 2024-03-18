757bedb7211f44047afcd9c5da9fccd4b2840f9b
Kendall Madrigal <kendallmadrigal14@gmail.com>
2024-03-18 08:57:33 -0600

    Agregando cambio en la lectura de la pantalla de administracion de cultivos

--- src/screens/administracion-cultivos/administracion-cultivos.tsx
+++ src/screens/administracion-cultivos/administracion-cultivos.tsx
@@ -5,14 +5,26 @@ import { BackButtonComponent } from '../../components/BackButton/BackButton';
 import { IconRectangle } from '../../components/IconRectangle/IconRectangle';
 import { Admin_cultivation } from '../../constants';
 import { ScreenProps } from '../../constants';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
 export const AdministracionCultivos: React.FC = () => {
+    const navigation = useNavigation<NativeStackNavigationProp<any>>();
 
     const handleBackPress = () => {
         console.log('Botón de retroceso presionado');
     };
 
-    const HandleRectanglePress = () => {
-        console.log('Rectangulo Presiona');
+    const HandleRectanglePress = (crops: any) => {
+        if (crops.screen !== '') {
+            if (crops.datoValidacion !== undefined) {
+                navigation.navigate(crops.screen, { datoValidacion: crops.datoValidacion });
+            } else {
+                navigation.navigate(crops.screen);
+            }
+        } else {
+            alert('Pantalla aún no disponible');
+        }
     }
 
     return (
@@ -24,12 +36,12 @@ export const AdministracionCultivos: React.FC = () => {
 
             <View style={styles.rowContainer}>
 
-                {Admin_cultivation.map((item) => (
-                    <View style={styles.row} key={item.id}>
+                {Admin_cultivation.map((crops) => (
+                    <View style={styles.row} key={crops.id}>
                         <IconRectangle
-                            onPress={HandleRectanglePress}
-                            iconImg={item.iconImage}
-                            text={item.text}
+                            onPress={() => HandleRectanglePress(crops)}
+                            iconImg={crops.iconImage}
+                            text={crops.text}
                         />
                     </View>
                 ))}
