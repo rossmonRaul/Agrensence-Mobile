7c0077dbda953a84ffa766f90ddd0bd31f47027d
Kendall Madrigal <kendallmadrigal14@gmail.com>
2024-03-18 08:58:01 -0600

    Servicios de cultivos para rotacion de cultivos

--- src/servicios/ServicioCultivos.ts
+++ src/servicios/ServicioCultivos.ts
@@ -3,10 +3,25 @@ const controlador = "Cultivos";
 import { API_URL } from "../constants";
 
 /*Metodos GET */
+export const ObtenerRotacionCultivoSegunEstacionalidad = async () => {
+    const url = `${API_URL}/api/v1.0/${controlador}/ObtenerRotacionCultivoSegunEstacionalidad`;
+    return await ProcesarDatosApi('GET', url, '');
+}
 
 
 /*Metodos POST */
+export const InsertarRotacionCultivoSegunEstacionalidad = async (data: any) => {
+    const url = `${API_URL}/api/v1.0/${controlador}/InsertarRotacionCultivoSegunEstacionalidad`;
+    return await ProcesarDatosApi('POST', url, data);
+}
 
+/*Metodos PUT */
+export const ModificarRotacionCultivoSegunEstacionalidad = async (data: any) => {
+    const url = `${API_URL}/api/v1.0/${controlador}/ModificarRotacionCultivoSegunEstacionalidad`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
 
-/*Metodos Metodos PUT */
-
+export const CambiarEstadoRotacionCultivo = async (data: any) => {
+    const url = `${API_URL}/api/v1.0/${controlador}/CambiarEstadoRotacionCultivo`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
