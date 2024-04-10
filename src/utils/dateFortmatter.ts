df3dd07be57c7d53ae6fceb5a0c00ee24558f8bd
Kendall Madrigal <kendallmadrigal14@gmail.com>
2024-04-10 13:35:22 -0600

    Formtaeador de fechas

---
+++ src/utils/dateFortmatter.ts
@@ -0,0 +1,7 @@
+export const formatSpanishDate = (date) => {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString().slice(-2);
+
+    return `${day}/${month}/${year}`;
+};
